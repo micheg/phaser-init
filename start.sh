20c2e967c8ef62542cf9e4ded4ea85b6787afdb0
micheg <michelangelog@gmail.com>
2019-02-19 16:11:09 +0100

    super init script

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/micheg/phaser-init/master/.gitignore
+curl -O https://raw.githubusercontent.com/micheg/phaser-init/master/Makefile
+curl -O https://raw.githubusercontent.com/micheg/phaser-init/master/README.md
+curl -O https://raw.githubusercontent.com/micheg/phaser-init/master/index.html
+curl -O https://raw.githubusercontent.com/micheg/phaser-init/master/init.sh
+curl -O https://raw.githubusercontent.com/micheg/phaser-init/master/release.html
